f140686ff4eab47826a05554e904cd026ac0acac
mtmcbride <michmcbr@indiana.edu>
2018-09-13 15:54:21 -0400

    lots of things

---
+++ extras/comparison.R
@@ -0,0 +1,13 @@
+load("manuscript/mazurka2results.Rdata")
+source('extras/djm_optimization.R')
+fliere = ContoR(unlist(pvec_ml[substr(rownames(pvec_ml), 1, 6) == "Fliere",]))
+tomsic = ContoR(unlist(pvec_ml[substr(rownames(pvec_ml), 1, 6) == "Tomsic",]))
+data("tempos")
+lt = diff(c(tempos$note_onset,61))
+yf = matrix(tempos$Fliere_1977,1)
+yt = matrix(tempos$Tomsic_1995, 1)
+out = array(dim = c(2,2))#each row is different parameters, each column is different performance
+out[1,1] = toOptimize(fliere, yf, lt, 200)
+out[2,1] = toOptimize(tomsic, yf, lt, 200)
+out[1,2] = toOptimize(fliere, yt, lt, 200)
+out[2,2] = toOptimize(tomsic, yt, lt, 200)
